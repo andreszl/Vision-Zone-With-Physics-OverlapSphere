bb08b0795032e4f787de07becfff9bb934de0fe6
andreszl <andreszl.devcode@gmail.com>
2021-09-27 06:40:52 -0500

    initial commit

---
+++ Assets/VisionZone.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+
+public class VisionZone : MonoBehaviour {
+    public float range = 10f;
+    public List<Collider> enemies;
+    private float lastId = 0;
+    public Collider[] elements;
+
+    void FixedUpdate() {
+        elements = Physics.OverlapSphere(transform.position, 10f);
+
+        foreach (var element in elements) {
+            if (element.tag == "Enemy") {
+
+                if (element.name == "Enemy") {
+                    AddEnemy(element);
+                } else {
+                    Collider value = enemies.Find(enemy => enemy.name == element.name);
+                    
+                    if (value == null) {
+                        AddEnemy(element);
+                    }
+                }
+            }
+        }
+    }
+
+    void AddEnemy(Collider enemy) {
+        lastId++;
+        enemy.name = "Enemy" + lastId;
+        enemies.Add(enemy);
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
